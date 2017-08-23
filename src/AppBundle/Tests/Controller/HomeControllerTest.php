5a7971e44e495e9290235bad1d61439b935c786e
Arnaud de Parseval <adeparseval@largus.fr>
2017-08-23 18:00:14 +0200

    configuration of project

--- src/AppBundle/Tests/Controller/HomeControllerTest.php
+++ src/AppBundle/Tests/Controller/HomeControllerTest.php
@@ -10,6 +10,7 @@ class HomeControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '');
+        $this->assertEquals(200,$client->getResponse()->getStatusCode());
     }
 
 }
