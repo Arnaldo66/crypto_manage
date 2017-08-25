75f5b225689db7892170dddfcc6ee8cbffcaf9cf
Arnaud de Parseval <adeparseval@largus.fr>
2017-08-25 18:04:01 +0200

    delete a unused command

--- src/AppBundle/Command/CreateCurrencyValueMomentCommand.php
+++ src/AppBundle/Command/CreateCurrencyValueMomentCommand.php
@@ -5,10 +5,13 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use AppBundle\Entity\Currency;
 use AppBundle\Entity\CurrencyValueMoment;
 
 class CreateCurrencyValueMomentCommand extends ContainerAwareCommand
 {
+    //TODO: have to transfert data in other table before truncate
+    // add validation verification before flush entity
 
     protected function configure()
     {
@@ -23,6 +26,8 @@ class CreateCurrencyValueMomentCommand extends ContainerAwareCommand
       $client = new \GuzzleHttp\Client();
       $res = $client->request('GET', 'https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=100');
       $em = $this->getContainer()->get('doctrine')->getManager();
+
+      //distroy all value
       $this->truncateTable($em);
 
       $output_message = 'KO';
@@ -47,28 +52,33 @@ class CreateCurrencyValueMomentCommand extends ContainerAwareCommand
 
     //Create new Currency if not exist. Base on currency name
     private function createCurrencyValueMoment($em,$value){
-        //TODO: create currency if not exists: have to create service currency and method currencyCreate
         $currency = $em->getRepository('AppBundle:Currency')->findOneByName($value->name);
 
-        if($currency !== NULL){
-          $currencyValueMoment = new CurrencyValueMoment;
-
-          $currencyValueMoment->setCurrency($currency);
-          $currencyValueMoment->setRank($value->rank);
-          $currencyValueMoment->setPriceUsd($value->price_usd);
-          $currencyValueMoment->setPriceBtc($value->price_btc);
-          $currencyValueMoment->setMarketCapUsd($value->market_cap_usd);
-          $currencyValueMoment->setAvailableSupply($value->available_supply);
-          $currencyValueMoment->setTotalSupply($value->total_supply);
-          $currencyValueMoment->setPercentChange1h($value->percent_change_1h);
-          $currencyValueMoment->setPercentChange24h($value->percent_change_24h);
-          $currencyValueMoment->setPercentChange7d($value->percent_change_7d);
-          $currencyValueMoment->setLastUpdated($value->last_updated);
-          $currencyValueMoment->setPriceEur($value->price_eur);
-          $currencyValueMoment->setMarketCapEur($value->percent_change_24h);
-
-          $em->persist($currencyValueMoment);
-          $em->flush();
+        //create currency if not exists
+        if($currency === NULL){
+          $currency = new Currency;
+          $currency->setName($value->name);
+          $currency->setSymbol($value->symbol);
+          $em->persist($currency);
         }
+
+        $currencyValueMoment = new CurrencyValueMoment;
+
+        $currencyValueMoment->setCurrency($currency);
+        $currencyValueMoment->setRank($value->rank);
+        $currencyValueMoment->setPriceUsd($value->price_usd);
+        $currencyValueMoment->setPriceBtc($value->price_btc);
+        $currencyValueMoment->setMarketCapUsd($value->market_cap_usd);
+        $currencyValueMoment->setAvailableSupply($value->available_supply);
+        $currencyValueMoment->setTotalSupply($value->total_supply);
+        $currencyValueMoment->setPercentChange1h($value->percent_change_1h);
+        $currencyValueMoment->setPercentChange24h($value->percent_change_24h);
+        $currencyValueMoment->setPercentChange7d($value->percent_change_7d);
+        $currencyValueMoment->setLastUpdated($value->last_updated);
+        $currencyValueMoment->setPriceEur($value->price_eur);
+        $currencyValueMoment->setMarketCapEur($value->percent_change_24h);
+
+        $em->persist($currencyValueMoment);
+        $em->flush();
     }
 }
