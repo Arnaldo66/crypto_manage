26c0c8c10e6c6ce09957f28d0276bfb9173b89e3
Arnaud de Parseval <adeparseval@largus.fr>
2017-10-10 17:07:45 +0200

    test order with bad value

--- src/AppBundle/Tests/Integration/TradingOrderTest.php
+++ src/AppBundle/Tests/Integration/TradingOrderTest.php
@@ -16,15 +16,19 @@ class TradingOrderTest extends WebTestCase
       ));
     }
 
-    public function testCreateOrder(){
-        $crawler = $this->client->request('GET', '/u/trade/order/new');
+    public function firstStep(){
+       $crawler = $this->client->request('GET', '/u/trade/order/new');
+       $buttonNode = $crawler->selectButton('btn-create-order');
+       $form = $buttonNode->form(array(
+           'trading_order_first_step[currency]'  => '1',
+       ));
+       $crawler = $this->client->submit($form);
+       $this->assertEquals(200,$this->client->getResponse()->getStatusCode());
+       return $crawler;
+    }
 
-        $buttonCrawlerNode = $crawler->selectButton('btn-create-order');
-        $form = $buttonCrawlerNode->form(array(
-            'trading_order_first_step[currency]'  => '1',
-        ));
-        $crawler = $this->client->submit($form);
-        $this->assertEquals(200,$this->client->getResponse()->getStatusCode());
+    public function testCreateOrder(){
+        $crawler = $this->firstStep();
 
         $buttonCrawlerNode = $crawler->selectButton('btn-finalise-order');
         $form = $buttonCrawlerNode->form(array(
@@ -39,6 +43,22 @@ class TradingOrderTest extends WebTestCase
         $this->assertTrue($this->client->getResponse()->isRedirect('/u/trade/wallets/1'));
         $crawler = $this->client->followRedirect();
         $this->assertCount(1, $crawler->filter('div.alert-success'));
+    }
+
+    public function testCreateWithoutValue(){
+        $crawler = $this->firstStep();
+
+        $buttonCrawlerNode = $crawler->selectButton('btn-finalise-order');
+        $form = $buttonCrawlerNode->form(array(
+            'trading_order_next_step[orderAction]'  => '1',
+            'trading_order_next_step[orderMethod]'  => '1',
+            'trading_order_next_step[tradingWallet]' => '1',
+            'trading_order_next_step[amount]' => '',
+            'trading_order_next_step[total]' => '',
+            'trading_order_next_step[price]' => '',
+        ));
+        $crawler = $this->client->submit($form);
+        $this->assertCount(3, $crawler->filter('span.error'));
     }
 
 }
