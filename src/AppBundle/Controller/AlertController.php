47a36dd0bc0fa3ad96643ebcabc1ff4bb25d88f0
Arnaud de Parseval <adeparseval@largus.fr>
2017-10-25 17:05:52 +0200

    form alert && new

--- src/AppBundle/Controller/AlertController.php
+++ src/AppBundle/Controller/AlertController.php
@@ -4,6 +4,10 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Form\AlertType;
+use AppBundle\Entity\Alert;
 
 class AlertController extends Controller
 {
@@ -23,11 +27,25 @@ class AlertController extends Controller
     /**
      * @Route("/u/alerts/new", name="alert_new")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        die('inew');
+        $alert = new Alert();
+        $alert->setUser($this->getUser());
+        $alert->setBuy(1);
+        $form = $this->createForm(AlertType::class, $alert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($alert);
+          $em->flush();
+
+          $this->addFlash('success-message','Votre alerte a bien été crée');
+          return $this->redirectToRoute('alert');
+        }
+
         return $this->render(':Alert:new.html.twig', array(
-            // ...
+            'form' => $form->createView()
         ));
     }
 
