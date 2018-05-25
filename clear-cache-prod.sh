06fae6cb61706f9acb89176b915e810e1b446e1e
Arnaud de Parseval <adeparseval@largus.fr>
2018-05-25 17:33:04 +0200

    sort by date

--- clear-cache-prod.sh
+++ clear-cache-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+php bin/console assets:install -e prod;
 php bin/console assetic:dump -e prod;
 php bin/console cache:clear --no-warmup -e prod;
-php bin/console doctrine:cache:clear-metadata -e prod;
