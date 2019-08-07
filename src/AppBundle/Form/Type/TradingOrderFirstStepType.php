4cf59cedb20c196c064610e62370596870bd285c
Arnaud <a.parseval@gmail.com>
2019-08-07 09:28:45 +0000

    max limite order

--- src/AppBundle/Form/Type/TradingOrderFirstStepType.php
+++ src/AppBundle/Form/Type/TradingOrderFirstStepType.php
@@ -5,8 +5,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use AppBundle\Entity\TradingOrder;
+use AppBundle\Entity\Currency;
 
 class TradingOrderFirstStepType extends AbstractType
 {
@@ -15,8 +17,13 @@ class TradingOrderFirstStepType extends AbstractType
     {
         $builder
             ->add('currency', EntityType::class, array(
-              'class' => 'AppBundle:Currency',
-              'choice_label' => 'name',
+                'class' => Currency::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                    ->orderBy('c.rank', 'ASC')
+                    ->setMaxResults(1000);
+                },
+                'choice_label' => 'name',
             ))
         ;
     }
