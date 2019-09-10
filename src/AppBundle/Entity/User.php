cf7e5c32271b9e2d9f5b9b31d39f6b292d093ded
Arnaud <a.parseval@gmail.com>
2019-09-10 15:29:43 +0000

    remove fos user and do just login with username

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,21 +3,42 @@
 
 namespace AppBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="currency_user")
  */
-class User extends BaseUser
+class User implements UserInterface, \Serializable
 {
     /**
-     * @ORM\Id
      * @ORM\Column(type="integer")
+     * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    protected $id;
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
 
     /**
      * @ORM\OneToMany(targetEntity="TradingWallet", mappedBy="user")
@@ -36,11 +57,56 @@ class User extends BaseUser
 
     public function __construct()
     {
-      parent::__construct();
-      $this->tradingWallets = new \Doctrine\Common\Collections\ArrayCollection();
-      $this->alerts = new \Doctrine\Common\Collections\ArrayCollection();
-      $this->myCryptoWallets = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->isActive = true;
+        $this->tradingWallets = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->alerts = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->myCryptoWallets = new \Doctrine\Common\Collections\ArrayCollection();
     }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->password,
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+        ) = unserialize($serialized, ['allowed_classes' => false]);
+    }
+
 
     /**
      * Add tradingWallet
